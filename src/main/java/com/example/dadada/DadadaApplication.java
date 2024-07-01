9268e46a4465420db6574717793321e46dc734bf
jj-kim4023 <jj.kim4023@gmail.com>
2024-07-01 23:16:16 +0900

    [chore]
    -프로젝트 생성 및 JWT 의존성 추가
    -Security Config 클래스

---
+++ src/main/java/com/example/dadada/DadadaApplication.java
@@ -0,0 +1,13 @@
+package com.example.dadada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadadaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DadadaApplication.class, args);
+    }
+
+}

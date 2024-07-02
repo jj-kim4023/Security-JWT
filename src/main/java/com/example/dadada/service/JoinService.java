320b4fea98b7915ac6c624a917a5922f558eabad
jj-kim4023 <jj.kim4023@gmail.com>
2024-07-02 22:49:29 +0900

    [chore]
    -DB 연결 및 Entity 생성
    -회원가입 로직 구현

---
+++ src/main/java/com/example/dadada/service/JoinService.java
@@ -0,0 +1,42 @@
+package com.example.dadada.service;
+
+import com.example.dadada.dto.JoinDTO;
+import com.example.dadada.entity.UserEntity;
+import com.example.dadada.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JoinService {
+
+    private final UserRepository userRepository;
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public JoinService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public void joinProcess(JoinDTO joinDTO) {
+
+        String username = joinDTO.getUsername();
+        String password = joinDTO.getPassword();
+
+        Boolean isExist = userRepository.existsByUsername(username);
+
+        if (isExist) {
+
+            return;
+        }
+
+        UserEntity data = new UserEntity();
+
+        data.setUsername(username);
+        data.setPassword(bCryptPasswordEncoder.encode(password));
+        data.setRole("ROLE_ADMIN");
+
+        userRepository.save(data);
+
+    }
+}

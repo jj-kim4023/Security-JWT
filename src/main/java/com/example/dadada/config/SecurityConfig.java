77ae3e4ac1d2652d8ea5dfcf3a17071c02787131
jj-kim4023 <jj.kim4023@gmail.com>
2024-07-04 23:25:01 +0900

    [chore] -로그인 필터 구현 -DB기반 로그인 검증 로직

--- src/main/java/com/example/dadada/config/SecurityConfig.java
+++ src/main/java/com/example/dadada/config/SecurityConfig.java
@@ -1,25 +1,42 @@
 package com.example.dadada.config;
 
+import com.example.dadada.jwt.LoginFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    public  SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+        this.authenticationConfiguration = authenticationConfiguration;
+
+    }
+    //AuthenticationManager Bean 등록
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptpasswordEncoder() {
 
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain FilterChain(HttpSecurity http) throws Exception {
 
         // csrf disable
         http
@@ -36,6 +53,8 @@ public class SecurityConfig {
                         .requestMatchers("login", "/", "/join").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().authenticated());
+        http
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
         // 세션 설정(중요)
         http
                 .sessionManagement((session) -> session
---
+++ src/main/java/com/example/dadada/service/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.example.dadada.service;
+
+import com.example.dadada.dto.CustomUserDetails;
+import com.example.dadada.entity.UserEntity;
+import com.example.dadada.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity userData = userRepository.findByUsername(username);
+
+        if (userData != null) {
+
+            return new CustomUserDetails(userData);
+
+        }
+
+        return null;
+    }
+}
